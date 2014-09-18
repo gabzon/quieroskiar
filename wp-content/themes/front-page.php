e9b1770ff9c96d54d53cff5c611b4d0b1dc97926
gabriel <gab.zambrano@gmail.com>
2014-09-18 10:34:29 +0200

    First commit

---
+++ wp-content/themes/front-page.php
@@ -0,0 +1,60 @@
+<div class="row">
+  <div class="span3">
+    <input type="search" class="form-control" placeholder="Rechercher">
+    <h4>Category</h4>
+      <div class="checkbox">
+        <input type="checkbox"> Family
+        <input type="checkbox"> Sport
+        <input type="checkbox"> Luxury
+        <input type="checkbox"> Shopping
+        <input type="checkbox"> Discount
+        <input type="checkbox"> Partying
+      </div>
+    <h4>Public transportation</h4>
+      <input type="checkbox"> Accesibility
+  </div>
+  <div class="span9">
+    <table class="table table-hover">
+      <tr>
+        <th>Station</th>
+        <th>State/Country</th>
+        <th>Domain</th>
+      </tr>
+      <tr>
+        <td><a href="http://www.champery.ch/">CHAMPERY</a></td>
+        <td>VS/CH</td>
+        <td>Les portes du soleil</td>
+      </tr>
+      <tr>
+        <td><a href="http://www.laax.com/">FLIMS LAAX FALERA</a></td>
+        <td>GR/CH</td>
+        <td>Grisons</td>
+      </tr>
+      <tr>
+        <td><a href="http://www.latzoumaz.ch/">LA TZOUMAZ</a></td>
+        <td>VS/CH</td>
+        <td>4 vallées</td>
+      </tr>
+      <tr>
+        <td><a href="http://gemeinde.bettmeralp.ch">BETTEN (Betttmeralp)</a></td>
+        <td>VS/CH</td>
+        <td>Aletsch-Arena</td>
+      </tr>
+      <tr>
+        <td><a href="http://www.sigriswil.ch/xml_1/internet/de/intro.cfm">SCHWANDEN - SIGRISWIL</a></td>
+        <td>BE/CH</td>
+        <td>Oberland Bernois</td>
+      </tr>
+      <tr>
+        <td><a href="http://www.myvalleedejoux.ch/fr/">L'ABBAYE</a></td>
+        <td>VD/CH</td>
+        <td>Jura vaudois</td>
+      </tr>
+      <tr>
+        <td><a href="http://www.villars.ch/en/">VILLARS</a></td>
+        <td>VD/CH</td>
+        <td>Alpes Vaudoises</td>
+      </tr>
+    </table>
+  </div>
+</div>
