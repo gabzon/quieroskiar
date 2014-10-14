988abbad355e07b373bfc93a0a5e1bd9388f1bb8
gabriel <gab.zambrano@gmail.com>
2014-10-14 15:19:47 +0200

    update content-table.php

--- wp-content/themes/roots/templates/content-table.php
+++ wp-content/themes/roots/templates/content-table.php
@@ -2,7 +2,7 @@
   <thead>
     <tr>
         <th>Resort</th>
-        <!-- <th>Webcam</th> -->
+        <th>Webcam</th>
         <th>Region</th>
         <th>Opening date</th>
         <th>Altitude Top</th>
@@ -13,7 +13,6 @@
   </thead>
 
   <tbody>
-
     <?php global $wp_query;
     $wp_query = new WP_Query("post_type=resort&post_status=publish");
     while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
@@ -28,9 +27,13 @@
         ?>
 
       <tr>
-        <td><a href="<?php echo get_post_meta($post->ID, 'website', true); ?>" target="_blank"><?php echo the_title(); ?></a></td>
-        <!-- <td><a href="#my_modal" data-toggle="modal" data-book-id="<?php //echo $post->ID; ?>"><?php //echo the_title(); ?></a></td> -->
-        <!-- <td><a href="<?php //echo $webcam;  ?>" target="_blank"><img src="<?php //echo $webcam  ?>" width="100"></a></td> -->
+        <td>
+          <a href="<?php echo get_post_meta($post->ID, 'website', true); ?>" target="_blank">
+            <?php echo get_the_post_thumbnail( $post->ID,'medium' ); ?>
+            <?php echo the_title(); ?>
+          </a>
+        </td>
+        <td><a href="<?php echo $webcam;  ?>" target="_blank"><img src="<?php echo $webcam  ?>" width="100"></a></td>
         <td><?php echo $region;?></td>
         <td><?php echo get_post_meta($post->ID, 'opening_date', true); ?></td>
         <td><?php echo get_post_meta($post->ID, 'altitude_top', true); ?></td>
@@ -46,7 +49,7 @@
   <tfoot>
     <tr>
         <th>Resort</th>
-        <!-- <th>Webcam</th> -->
+        <th>Webcam</th>
         <th>Region</th>
         <th>Opening date</th>
         <th>Altitude Top</th>
@@ -61,20 +64,8 @@
 
 <script type="text/javascript" language="javascript" class="init">
 $(document).ready(function() {
-  var bookId = '';
 
   $('#example').dataTable();
-
-  $('#my_modal').on('show.bs.modal', function(e) {
-    var bookId = $(e.relatedTarget).data('book-id');
-    $(e.currentTarget).find('input[name="bookId"]').val(bookId);
-    $.ajax({
-      //url: 'http://localhost/want2ski/',
-      url: 'modal-resort.php',
-      type: 'get',
-      data: { x: 'dsd' }
-    });
-  });
 
 });
 </script>
